e83e1959c117839d59447a35a3565954aebfebb5
Reda M <reda.madjoub@gmail.com>
2021-09-12 18:28:04 +0200

    ajout du contenu dynamique du menu accordeon

--- src/component/About.js
+++ src/component/About.js
@@ -3,13 +3,40 @@ import Accordion from './Accordion'
 import mountain from '../img/MOUNTAIN.png'
 import './About.css'
 
+
+const allValues = {
+    1: {
+        nameValue: "Fiabilité",
+        descriptionValue: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    },
+    2: {
+        nameValue: "Respect",
+        descriptionValue: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    3: {
+        nameValue: "Service",
+        descriptionValue: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    },
+    4: {
+        nameValue: "Sécurité",
+        descriptionValue: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    }
+}
 export default class About extends Component {
+    state = {
+        allValues,
+    }
     render() {
+        const liste = Object.keys(allValues).map(el => 
+            <Accordion 
+                name={allValues[el].nameValue} 
+                description={allValues[el].descriptionValue}    
+            />)
         return (
-            <div>
-                <img src={ mountain } alt='Mountain'/>
-                <Accordion />
-            </div>
+            <main className="ourValues">
+                <img src={mountain} alt='Mountain '/>
+                { liste }
+            </main>
         )
     }
 }
--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import './Accordion.css'
+import arrow from '../img/arrow_dropdown.svg'
 
 export default class Accordion extends Component {
     render() {
         return (
-            <div>
-                <h1>Accordion</h1>
-            </div>
+            <article className="principle">
+                <div className="principleHeader">
+                    <p>{this.props.name}</p>
+                    <img src={arrow} alt="arrow to open and close element view"/>
+                </div>
+                <div className="principleContent">
+                    <p>{this.props.description}</p>
+                </div>
+            </article>
         )
     }
 }
