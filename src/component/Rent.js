8249117d0e1952c6a01e5bc445f5ac236982b0ea
Reda M <reda.madjoub@gmail.com>
2021-11-07 10:24:26 +0100

    fix bug react-router version

--- src/component/Rent.js
+++ src/component/Rent.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import React, { Component, useParams } from 'react'
+import { NavLink  } from 'react-router-dom'
 import './Rent.css'
 import {data} from '../data/data.js'
 import Accordion from './Accordion'
@@ -9,13 +9,14 @@ import Tags from './Tags'
 import Title from './Title'
 import Slider from './Slider'
 
-
 export default class Rent extends Component {
     getRent = () => data.filter(obj => obj.id === this.props.match.params.id)
     render() {
+        console.log(this.props.match.params.id);
+        // console.log(this.getRent())
         const checkId = data.filter(elt => elt.id === this.props.match.params.id)
-        if(!checkId[0]) return <Redirect to="/error" />
-        if(this.props.match.params.id !== checkId[0].id ) return <Redirect to="/error" />
+        if(!checkId[0]) return <NavLink  to="/error" />
+        if(this.props.match.params.id !== checkId[0].id ) return <NavLink  to="/error" />
         const {
             description,
             equipments,
@@ -26,9 +27,11 @@ export default class Rent extends Component {
             tags,
             title
          } = this.getRent()[0]
-
+         console.log(host);
         return (
+            
             <div className="rent">
+                {console.log(this.props)}
                 <Slider pictures={pictures}/>
             <div className="contentRent">
                 <div className="textInfo">
@@ -56,3 +59,4 @@ export default class Rent extends Component {
         )
     }
 }
+
